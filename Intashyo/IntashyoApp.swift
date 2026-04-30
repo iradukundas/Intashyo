2a2c1448788179def8535a378548e36d8f933458
iradukundas20 <iradukuunda@gmail.com>
2026-04-30 16:19:40 -0400

    Initial Commit

---
+++ Intashyo/IntashyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntashyoApp.swift
+//  Intashyo
+//
+//  Created by Samuel Iradukunda on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct IntashyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntashyoTests/IntashyoTests.swift
@@ -0,0 +1,19 @@
+//
+//  IntashyoTests.swift
+//  IntashyoTests
+//
+//  Created by Samuel Iradukunda on 4/30/26.
+//
+
+import Testing
+@testable import Intashyo
+
+struct IntashyoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
